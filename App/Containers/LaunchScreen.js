e494491d8c48211cda5eabfc614a9c7c347151f2
yarikgenza <gsmeter@gmail.com>
2017-03-19 11:56:41 +0200

    configured basic launchScreen

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,29 +1,25 @@
-import React from 'react'
-import { ScrollView, Text, Image, View } from 'react-native'
+import React, { Component } from 'react'
+import { Text, Image, View } from 'react-native'
 import { Images } from '../Themes'
 
 // Styles
 import styles from './Styles/LaunchScreenStyles'
 
-export default class LaunchScreen extends React.Component {
+export default class LaunchScreen extends Component {
 
   render () {
     return (
-      <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
-        <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
-          </View>
-
-          <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              {"This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite."}
-            </Text>
-          </View>
-
-        </ScrollView>
+      <View style={styles.container}>
+        <View style={styles.logoBlock}>
+          <Image
+            style={styles.logo}
+            source={Images.logo}
+          />
+        </View>
+        <View style={styles.headingContainer}>
+          <Text style={styles.heading}>Where to go?</Text>
+          <Text style={styles.headingName}>Lviv</Text>
+        </View>
       </View>
     )
   }
