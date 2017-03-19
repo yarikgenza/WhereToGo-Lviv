d3e148275448793a018fbe41926d87632d259a64
yarikgenza <gsmeter@gmail.com>
2017-03-19 12:40:15 +0200

    added "next screen" button

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
-import { Text, Image, View } from 'react-native'
-import { Images } from '../Themes'
+import React, { Component } from 'react';
+import { Text, Image, View } from 'react-native';
+import NativeFeedbackButton from '../Components/NativeFeedbackButton';
+import { Images } from '../Themes';
 
 // Styles
 import styles from './Styles/LaunchScreenStyles'
 
 export default class LaunchScreen extends Component {
+
+  handleNextButtonPress() {
+    alert('hello)')
+  }
 
   render () {
     return (
@@ -20,6 +25,11 @@ export default class LaunchScreen extends Component {
           <Text style={styles.heading}>Where to go?</Text>
           <Text style={styles.headingName}>Lviv</Text>
         </View>
+        <NativeFeedbackButton
+          onPress={() => this.handleNextButtonPress}
+          title='start exploring now'
+          styles={styles.nextButton}
+          textStyles={styles.buttonText} />
       </View>
     )
   }
