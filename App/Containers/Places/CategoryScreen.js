9facb9314d2d319e683b7ef7d4588df90d76594c
yarikgenza <gsmeter@gmail.com>
2017-03-31 16:53:54 +0300

    ee

---
+++ App/Containers/Places/CategoryScreen.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native';
+
+export default class CategoryScreen extends Component {
+  render() {
+    return(
+      <View>
+        <Text>Hello world</Text>
+        <Text>Hello world</Text>
+        <Text>Hello world</Text>
+        <Text>Hello world</Text>
+        <Text>Hello world</Text>
+        <Text>Hello world</Text>
+        <Text>Hello world</Text>
+        <Text>Hello world</Text>
+        <Text>Hello world</Text>
+        <Text>Hello world</Text>
+        <Text>Hello world</Text>
+        <Text>Hello world</Text>
+        <Text>Hello world</Text>
+        <Text>Hello world</Text>
+        <Text>Hello world</Text>
+        
+      </View>
+    )
+  }
+}
