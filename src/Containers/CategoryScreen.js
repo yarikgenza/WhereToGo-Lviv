3654892b3ca486a6406f5f87f6c5eb7ce4d8a324
yarikgenza <gsmeter@gmail.com>
2017-04-11 15:46:19 +0300

    animations

--- src/Containers/CategoryScreen.js
+++ src/Containers/CategoryScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { TouchableOpacity, View } from 'react-native';
 import { Grid, Col, Row } from 'react-native-easy-grid';
 import { Container, Content, Text, Icon, Card } from 'native-base';
+import * as Animatable from 'react-native-animatable';
 import vars from '../Themes/variables/platform';
 import styles from './Styles/CategoryScreenStyles';
 
@@ -32,10 +33,12 @@ class CategoryScreen extends Component {
   render() {
     const renderCards = () => cards.map((card, index) => (
       <Card key={index} style={{ alignItems: 'center', justifyContent: 'center', borderColor: 'transparent', backgroundColor: vars.defaultBackgroundColor }}>
-        <TouchableOpacity onPress={() => this.handlePress(card.code)}>
-          <Icon name={card.icon} style={{ alignSelf: 'center', fontSize: 50, color: vars.defaultOrangeTextColor, padding: 7 }} />
-          <Text style={{ alignSelf: 'center', color: 'white' }}>{ card.title }</Text>
-        </TouchableOpacity>
+        <Animatable.View animation="swing" delay={600} duration={1500} iterationCount={1}>
+          <TouchableOpacity onPress={() => this.handlePress(card.code)}>
+            <Icon name={card.icon} style={{ alignSelf: 'center', fontSize: 50, color: vars.defaultOrangeTextColor, padding: 7 }} />
+            <Text style={{ alignSelf: 'center', color: 'white' }}>{ card.title }</Text>
+          </TouchableOpacity>
+        </Animatable.View>
       </Card>
       ));
 
@@ -45,8 +48,12 @@ class CategoryScreen extends Component {
 
           <Row size={3}>
             <View style={styles.headingContainer}>
-              <Text style={{ color: 'white', fontSize: 44, padding: 10 }}><Text style={{ fontSize: 44, color: vars.defaultOrangeTextColor }}>Where</Text> would you</Text>
-              <Text style={{ color: 'white', fontSize: 44 }}>love <Text style={{ fontSize: 44, color: vars.defaultOrangeTextColor }}>to go</Text>?</Text>
+              <Animatable.View animation="slideInLeft" duration={1500} delay={300} iterationCount={1} useNativeDriver>
+                <Text style={{ color: 'white', fontSize: 44, padding: 10 }}><Text style={{ fontSize: 44, color: vars.defaultOrangeTextColor }}>Where</Text> would you</Text>
+              </Animatable.View>
+              <Animatable.View animation="slideInRight" iterationCount={1} useNativeDriver>
+                <Text style={{ color: 'white', fontSize: 44 }}>love <Text style={{ fontSize: 44, color: vars.defaultOrangeTextColor }}>to go</Text>?</Text>
+              </Animatable.View>
             </View>
           </Row>
 
