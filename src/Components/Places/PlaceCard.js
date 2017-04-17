54387cfac1d38931477a7bf417d4c790d9e4a67e
yarikgenza <gsmeter@gmail.com>
2017-04-17 16:37:43 +0300

    distance api

--- src/Components/Places/PlaceCard.js
+++ src/Components/Places/PlaceCard.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import ResponsiveImage from 'react-native-responsive-image';
 import { Card, CardItem, Text, Button, Icon } from 'native-base';
+import { connect } from 'react-redux';
 import { _getPhotoUrl } from '../../Api/places.api';
+import { getDistance as _getDistance } from '../../Api/distance.api';
 import styles from './PlaceCardStyles';
 import StarRating from '../StarRating';
 
 class PlaceCard extends Component {
 
+  constructor() {
+    super();
+  }
+
   loadImage(place) {
     return place.photos ? _getPhotoUrl(place.photos[0].photo_reference) : null;
+  }
+
+  getDistance() {
+    const { location, place } = this.props;
+    const origin = `${location.latitude},${location.longitude}`;
+    const destination = `${place.geometry.location.lat},${place.geometry.location.lng}`;
+    _getDistance(origin, destination)
+      .then(result => alert(result))
+      .catch(e => alert(e));
+  }
+
+
+  componentDidMount() {
+    this.getDistance();
   }
 
   render() {
@@ -40,4 +60,10 @@ class PlaceCard extends Component {
   }
 }
 
-export default PlaceCard;
+const mapStateToProps = state => ({
+  location: state.location,
+});
+
+const mapDispatchToProps = () => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PlaceCard);
